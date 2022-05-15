66904db74e9f4631871261bbf06ae43d69fca97c
L <luenwarneke@gmail.com>
2022-05-15 19:23:16 +1000

    .

---
+++ script.js
@@ -0,0 +1,38 @@
+
+const default = ['#wanderstories','#townsvilleadventures','#townsvilleshines','#qldparks','#todayintownsville']
+const general = ['#thisisqueensland','#seeaustralia','#explorequeensland','#exploreaustralia','#queensland_captures','#thisisqueensland','#visitqueensland','#adventurethatislife']
+const location = {
+  nq : ['#townsville','#townsvillelocal','#townsvillelife','#nqadventures','#northqueensland','#thisistownsville','#northqueenslandlife'],
+  fnq : ['#exploretnq','#tropicalnorthqueensland','#fnq','#fnqlife'],
+  gbr : []
+}
+const activity = {
+  waterfalls : {
+    default : ['#waterfallsofqueensland','#waterfallwednesday','#chasingwaterfalls']
+  },
+  hiking : {
+    default : ['#hike_australia','#wildearthaustralia','#hikeaustralia','#hike'],
+    nq : ['#hikenorthqueensland','#townsvillehiking']
+  },
+  climbing : {
+    default : ['#townsvilleclimbing','#womenwhoclimb']
+  },
+  biking : {
+    default : ['#mountainbiking'],
+    nq : ['#rockwheelersmtbclub']
+  },
+  kayaking : {
+    default : ['#kayaking'],
+    nq : ['#townsvillekayakclub']
+  },
+  highlining : {
+    default : ['#hownottohighline','#highlining'],
+    nq : ['#townsvillehighlining']
+  }
+}
+const accounts = {
+  waterfalls : ['@creek.of.the.week','@waterfallwednesday','@queenslandwaterfalls'],
+  qld : ['@visitqueensland','@qldparks','@discoverqueensland','@travelqld'],
+  nq : ['@townsvillenorthqueensland','@lifeintownsville'],
+  fnq : ['@tropicalnorthqueensland','@tropicalcoastqueensland','@cassowarycoastregion']
+}
